e60e04d1e5d96ae20aab63de146e23d9b99c0f90
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2025-11-17 00:40:05 +0530

    feat: delete books flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,6 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// middleware
 app.use(cors());
 app.use(express.json()); // parses JSON request bodies
 app.use(express.urlencoded({ extended: true })); // parses form data
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -43,6 +43,79 @@ router.post("/", protectRoute, async (req, res) => {
 });
 
 //get all books
+//pagination -> infinite scrolling
+// fetch("http://localhost:3000/api/books?page=3&limit=5")
+router.get("/", async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort(createdAt - 1) // desc order
+      .skip(skip)
+      .limit(limit)
+      .populate("user", " username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit), // fields for FE
+    });
+    // default status code is 200
+  } catch (error) {
+    console.log("Error in get books", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//get recommended books by the logged in user
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.json(books);
+  } catch (error) {
+    console.log("Get user books error:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 //delete a book
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    // checking if user is the creator of a book
+    if (book.user.toString() !== req.user_id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // delete image from cloudinary
+    // sample url: https://res.cloudanary.com/image/upload/v188383849/qhuruuejhe.png ...
+    if ((book.image && book, image.includes("cloudanary"))) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (error) {
+        console.log("Error deleting image on cloudinary", error);
+      }
+    }
+
+    await book.deleteOne();
+
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting a book", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export default router;
